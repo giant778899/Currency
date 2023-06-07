fa0f0cc7125a7823c1f24f50a7c7729ac456bc72
giant778899 <richi778899@gmail.com>
2023-06-07 16:56:10 +0800

    commitTest

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -2,13 +2,35 @@ package com.home.currency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText ntd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ntd = findViewById(R.id.ntd);
+    }
+    public void count(View view){
+        if (ntd.getText().toString().equals("")){
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter you NTD amount")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }else {
+            float result = Float.parseFloat(ntd.getText().toString()) / 30.9f;
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is " + result)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
     }
 }
